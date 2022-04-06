20960bf52541231dd21feed014789d212933e8b9
Yousif Hawash <yousifh3067@hstat.org>
2022-04-06 17:17:01 +0000

    added light/dark mode java

---
+++ script.js
@@ -0,0 +1,10 @@
+document.querySelector('#dark-theme-btn').addEventListener('click',function(){
+  document.body.style.backgroundColor = "#333";
+//    ('--background-color, '#333')
+});
+ 
+ 
+document.querySelector('#light-theme-btn').addEventListener('click',function(){
+  document.body.style.backgroundColor = "white";
+//    ('--background-color, '#white')
+});
